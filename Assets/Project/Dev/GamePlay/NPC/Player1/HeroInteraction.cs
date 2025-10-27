0f474f8003cfda7e1c67ce3c1f6e1c862da0ee15
Rodion3000 <rodion.papizh@mail.ru>
2025-10-27 15:31:20 +0500

    1

--- Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
+++ Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
@@ -5,11 +5,11 @@ namespace Project.Dev.GamePlay.NPC.Player1
 {
     public class HeroInteraction : MonoBehaviour
     {
-        [SerializeField] private Transform holdPoint;
         [SerializeField] private float pickupDistance = 3f;
         [SerializeField] private LayerMask interactableLayer;
 
         private Rigidbody heldObject;
+        private FixedJoint joint;
         private bool isHolding;
 
         void Update()
@@ -21,21 +21,29 @@ namespace Project.Dev.GamePlay.NPC.Player1
                 else
                     TryPickupObject();
             }
-
         }
 
         private void TryPickupObject()
         {
             Transform camTransform = Camera.main.transform;
             Ray ray = new Ray(camTransform.position, camTransform.forward);
+
             if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
             {
                 Rigidbody rb = hit.collider.attachedRigidbody;
                 if (rb != null)
                 {
                     heldObject = rb;
-                    heldObject.isKinematic = true;
-                    heldObject.transform.SetParent(camTransform);
+                    heldObject.useGravity = false;
+                    heldObject.isKinematic = false;
+                    heldObject.velocity = Vector3.zero;
+                    heldObject.angularVelocity = Vector3.zero;
+
+                    joint = camTransform.gameObject.AddComponent<FixedJoint>();
+                    joint.connectedBody = heldObject;
+                    joint.breakForce = 1000f;
+                    joint.breakTorque = 1000f;
+
                     isHolding = true;
                 }
             }
@@ -43,10 +51,15 @@ namespace Project.Dev.GamePlay.NPC.Player1
 
         private void DropObject()
         {
+            if (joint != null)
+            {
+                Destroy(joint);
+                joint = null;
+            }
+
             if (heldObject != null)
             {
-                heldObject.isKinematic = false;
-                heldObject.transform.SetParent(null);
+                heldObject.useGravity = true;
                 heldObject = null;
                 isHolding = false;
             }
