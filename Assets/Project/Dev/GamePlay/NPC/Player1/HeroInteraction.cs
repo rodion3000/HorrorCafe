ecc0462012cb72deea8859a2fdb868df74c96ee9
Rodion3000 <rodion.papizh@mail.ru>
2025-10-28 10:42:52 +0500

    1

--- Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
+++ Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
@@ -5,97 +5,109 @@ using UnityEngine.InputSystem;
 namespace Project.Dev.GamePlay.NPC.Player1
 {
     public class HeroInteraction : MonoBehaviour
+{
+    [Header("Основные настройки")]
+    [SerializeField] private float pickupDistance = 3f;
+    [SerializeField] private LayerMask interactableLayer;
+
+    [Header("Физика удержания")]
+    [SerializeField] private float holdDistance = 1.5f;
+    [SerializeField] private float jointSpring = 2000f;   // сила "притяжения"
+    [SerializeField] private float jointDamping = 80f;    // сглаживание
+    [SerializeField] private float maxForce = 2000f;      // ограничение силы
+
+    private Rigidbody heldObject;
+    private ConfigurableJoint heldJoint;
+    private bool isHolding;
+    private Camera cam;
+
+    private void Start()
     {
-        [SerializeField] private float pickupDistance = 3f;
-        [SerializeField] private LayerMask interactableLayer;
-        [SerializeField] private float jointSpring = 5000f; // сила притяжения
-        [SerializeField] private float jointDamping = 100f; // сглаживание
+        cam = Camera.main;
+    }
 
-        private Rigidbody heldObject;
-        private ConfigurableJoint heldJoint;
-        private bool isHolding;
-        private Camera cam;
-
-        private void Start()
+    private void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            cam = Camera.main;
-        }
-
-        void Update()
-        {
-            if (Keyboard.current.eKey.wasPressedThisFrame)
-            {
-                if (isHolding)
-                    DropObject();
-                else
-                    TryPickupObject();
-            }
-
-            // обновляем точку привязки (камера)
-            if (isHolding && heldJoint != null)
-            {
-                heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * 1.5f;
-            }
-        }
-
-        private void TryPickupObject()
-        {
-            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-
-            if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
-            {
-                Rigidbody rb = hit.collider.attachedRigidbody;
-                if (rb != null)
-                {
-                    heldObject = rb;
-                    heldObject.useGravity = true;
-                    heldObject.isKinematic = false;
-
-                    // создаём joint НА ПРЕДМЕТЕ
-                    heldJoint = heldObject.gameObject.AddComponent<ConfigurableJoint>();
-                    heldJoint.connectedBody = null; // соединяем не с rigidbody, а с world anchor
-                    heldJoint.autoConfigureConnectedAnchor = false;
-                    heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * 1.5f;
-
-                    // блокируем вращение, но оставляем движение
-                    heldJoint.xMotion = ConfigurableJointMotion.Limited;
-                    heldJoint.yMotion = ConfigurableJointMotion.Limited;
-                    heldJoint.zMotion = ConfigurableJointMotion.Limited;
-                    heldJoint.angularXMotion = ConfigurableJointMotion.Locked;
-                    heldJoint.angularYMotion = ConfigurableJointMotion.Locked;
-                    heldJoint.angularZMotion = ConfigurableJointMotion.Locked;
-
-                    // настраиваем мягкую пружину
-                    JointDrive drive = new JointDrive
-                    {
-                        positionSpring = jointSpring,
-                        positionDamper = jointDamping,
-                        maximumForce = Mathf.Infinity
-                    };
-
-                    heldJoint.xDrive = drive;
-                    heldJoint.yDrive = drive;
-                    heldJoint.zDrive = drive;
-
-                    isHolding = true;
-                }
-            }
-        }
-
-        private void DropObject()
-        {
-            if (heldJoint != null)
-            {
-                Destroy(heldJoint);
-            }
-
-            if (heldObject != null)
-            {
-                heldObject.useGravity = true;
-                heldObject = null;
-            }
-
-            isHolding = false;
+            if (isHolding)
+                DropObject();
+            else
+                TryPickupObject();
         }
     }
+
+    private void FixedUpdate()
+    {
+        // Обновляем положение якоря в FixedUpdate, чтобы не было jitter (дрожи)
+        if (isHolding && heldJoint != null)
+        {
+            Vector3 targetAnchor = cam.transform.position + cam.transform.forward * holdDistance;
+            heldJoint.connectedAnchor = Vector3.Lerp(heldJoint.connectedAnchor, targetAnchor, 0.5f);
+        }
+    }
+
+    private void TryPickupObject()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
+        {
+            Rigidbody rb = hit.collider.attachedRigidbody;
+            if (rb != null)
+            {
+                heldObject = rb;
+                heldObject.useGravity = true;
+                heldObject.isKinematic = false;
+                heldObject.drag = 8f;           // сглаживаем движение
+                heldObject.angularDrag = 8f;
+                heldObject.constraints = RigidbodyConstraints.FreezeRotation;
+
+                // создаём joint на предмете
+                heldJoint = heldObject.gameObject.AddComponent<ConfigurableJoint>();
+                heldJoint.autoConfigureConnectedAnchor = false;
+                heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * holdDistance;
+
+                // ограничиваем движение, но не полностью фиксируем
+                heldJoint.xMotion = ConfigurableJointMotion.Limited;
+                heldJoint.yMotion = ConfigurableJointMotion.Limited;
+                heldJoint.zMotion = ConfigurableJointMotion.Limited;
+                heldJoint.angularXMotion = ConfigurableJointMotion.Locked;
+                heldJoint.angularYMotion = ConfigurableJointMotion.Locked;
+                heldJoint.angularZMotion = ConfigurableJointMotion.Locked;
+
+                // устанавливаем мягкую “пружину” с ограничением силы
+                JointDrive drive = new JointDrive
+                {
+                    positionSpring = jointSpring,
+                    positionDamper = jointDamping,
+                    maximumForce = maxForce
+                };
+
+                heldJoint.xDrive = drive;
+                heldJoint.yDrive = drive;
+                heldJoint.zDrive = drive;
+
+                isHolding = true;
+            }
+        }
+    }
+
+    private void DropObject()
+    {
+        if (heldJoint != null)
+        {
+            Destroy(heldJoint);
+        }
+
+        if (heldObject != null)
+        {
+            heldObject.drag = 0f;
+            heldObject.angularDrag = 0.05f;
+            heldObject.constraints = RigidbodyConstraints.None;
+            heldObject = null;
+        }
+
+        isHolding = false;
+    }
+}
 }
