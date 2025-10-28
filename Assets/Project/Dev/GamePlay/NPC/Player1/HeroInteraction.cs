c436dc3afc867c506e5291ce89e8feccfe4787b4
Rodion3000 <rodion.papizh@mail.ru>
2025-10-28 11:12:39 +0500

    1

--- Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
+++ Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
@@ -5,109 +5,153 @@ using UnityEngine.InputSystem;
 namespace Project.Dev.GamePlay.NPC.Player1
 {
     public class HeroInteraction : MonoBehaviour
-{
-    [Header("Основные настройки")]
-    [SerializeField] private float pickupDistance = 3f;
-    [SerializeField] private LayerMask interactableLayer;
-
-    [Header("Физика удержания")]
-    [SerializeField] private float holdDistance = 1.5f;
-    [SerializeField] private float jointSpring = 2000f;   // сила "притяжения"
-    [SerializeField] private float jointDamping = 80f;    // сглаживание
-    [SerializeField] private float maxForce = 2000f;      // ограничение силы
-
-    private Rigidbody heldObject;
-    private ConfigurableJoint heldJoint;
-    private bool isHolding;
-    private Camera cam;
-
-    private void Start()
     {
-        cam = Camera.main;
-    }
+        [Header("Основные настройки")] [SerializeField]
+        private float pickupDistance = 3f;
 
-    private void Update()
-    {
-        if (Keyboard.current.eKey.wasPressedThisFrame)
+        [SerializeField] private LayerMask interactableLayer;
+
+        [Header("Физика удержания")] [SerializeField]
+        private float holdDistance = 1.5f;
+
+        [SerializeField] private float jointSpring = 2000f;
+        [SerializeField] private float jointDamping = 80f;
+        [SerializeField] private float maxForce = 2000f;
+
+        [Header("Подсветка объекта")] [SerializeField]
+        private Color highlightColor = Color.yellow;
+
+        private Rigidbody heldObject;
+        private ConfigurableJoint heldJoint;
+        private bool isHolding;
+        private Camera cam;
+
+        private Renderer highlightedRenderer;
+        private Color originalColor;
+
+        private void Start()
         {
-            if (isHolding)
-                DropObject();
-            else
-                TryPickupObject();
+            cam = Camera.main;
         }
-    }
 
-    private void FixedUpdate()
-    {
-        // Обновляем положение якоря в FixedUpdate, чтобы не было jitter (дрожи)
-        if (isHolding && heldJoint != null)
+        private void Update()
         {
-            Vector3 targetAnchor = cam.transform.position + cam.transform.forward * holdDistance;
-            heldJoint.connectedAnchor = Vector3.Lerp(heldJoint.connectedAnchor, targetAnchor, 0.5f);
-        }
-    }
+            HandleHighlight();
 
-    private void TryPickupObject()
-    {
-        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
-        {
-            Rigidbody rb = hit.collider.attachedRigidbody;
-            if (rb != null)
+            if (Keyboard.current.eKey.wasPressedThisFrame)
             {
-                heldObject = rb;
-                heldObject.useGravity = true;
-                heldObject.isKinematic = false;
-                heldObject.drag = 8f;           // сглаживаем движение
-                heldObject.angularDrag = 8f;
-                heldObject.constraints = RigidbodyConstraints.FreezeRotation;
-
-                // создаём joint на предмете
-                heldJoint = heldObject.gameObject.AddComponent<ConfigurableJoint>();
-                heldJoint.autoConfigureConnectedAnchor = false;
-                heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * holdDistance;
-
-                // ограничиваем движение, но не полностью фиксируем
-                heldJoint.xMotion = ConfigurableJointMotion.Limited;
-                heldJoint.yMotion = ConfigurableJointMotion.Limited;
-                heldJoint.zMotion = ConfigurableJointMotion.Limited;
-                heldJoint.angularXMotion = ConfigurableJointMotion.Locked;
-                heldJoint.angularYMotion = ConfigurableJointMotion.Locked;
-                heldJoint.angularZMotion = ConfigurableJointMotion.Locked;
-
-                // устанавливаем мягкую “пружину” с ограничением силы
-                JointDrive drive = new JointDrive
-                {
-                    positionSpring = jointSpring,
-                    positionDamper = jointDamping,
-                    maximumForce = maxForce
-                };
-
-                heldJoint.xDrive = drive;
-                heldJoint.yDrive = drive;
-                heldJoint.zDrive = drive;
-
-                isHolding = true;
+                if (isHolding)
+                    DropObject();
+                else
+                    TryPickupObject();
             }
         }
-    }
 
-    private void DropObject()
-    {
-        if (heldJoint != null)
+        private void FixedUpdate()
         {
-            Destroy(heldJoint);
+            // Обновляем точку удержания (без дрожи)
+            if (isHolding && heldJoint != null)
+            {
+                Vector3 targetAnchor = cam.transform.position + cam.transform.forward * holdDistance;
+                heldJoint.connectedAnchor = Vector3.Lerp(heldJoint.connectedAnchor, targetAnchor, 0.5f);
+            }
         }
 
-        if (heldObject != null)
+        private void HandleHighlight()
         {
-            heldObject.drag = 0f;
-            heldObject.angularDrag = 0.05f;
-            heldObject.constraints = RigidbodyConstraints.None;
-            heldObject = null;
+            if (isHolding)
+            {
+                ClearHighlight();
+                return;
+            }
+
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
+            {
+                Renderer r = hit.collider.GetComponent<Renderer>();
+                if (r != null)
+                {
+                    if (highlightedRenderer != r)
+                    {
+                        ClearHighlight();
+                        highlightedRenderer = r;
+                        originalColor = r.material.color;
+                        r.material.color = highlightColor;
+                    }
+                }
+            }
+            else
+            {
+                ClearHighlight();
+            }
         }
 
-        isHolding = false;
+        private void ClearHighlight()
+        {
+            if (highlightedRenderer != null)
+            {
+                highlightedRenderer.material.color = originalColor;
+                highlightedRenderer = null;
+            }
+        }
+
+        private void TryPickupObject()
+        {
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
+            {
+                Rigidbody rb = hit.collider.attachedRigidbody;
+                if (rb != null)
+                {
+                    ClearHighlight(); // убрать подсветку при подборе
+                    heldObject = rb;
+                    heldObject.useGravity = true;
+                    heldObject.isKinematic = false;
+                    heldObject.drag = 8f;
+                    heldObject.angularDrag = 8f;
+                    heldObject.constraints = RigidbodyConstraints.FreezeRotation;
+
+                    heldJoint = heldObject.gameObject.AddComponent<ConfigurableJoint>();
+                    heldJoint.autoConfigureConnectedAnchor = false;
+                    heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * holdDistance;
+
+                    heldJoint.xMotion = ConfigurableJointMotion.Limited;
+                    heldJoint.yMotion = ConfigurableJointMotion.Limited;
+                    heldJoint.zMotion = ConfigurableJointMotion.Limited;
+                    heldJoint.angularXMotion = ConfigurableJointMotion.Locked;
+                    heldJoint.angularYMotion = ConfigurableJointMotion.Locked;
+                    heldJoint.angularZMotion = ConfigurableJointMotion.Locked;
+
+                    JointDrive drive = new JointDrive
+                    {
+                        positionSpring = jointSpring,
+                        positionDamper = jointDamping,
+                        maximumForce = maxForce
+                    };
+
+                    heldJoint.xDrive = drive;
+                    heldJoint.yDrive = drive;
+                    heldJoint.zDrive = drive;
+
+                    isHolding = true;
+                }
+            }
+        }
+
+        private void DropObject()
+        {
+            if (heldJoint != null)
+                Destroy(heldJoint);
+
+            if (heldObject != null)
+            {
+                heldObject.drag = 0f;
+                heldObject.angularDrag = 0.05f;
+                heldObject.constraints = RigidbodyConstraints.None;
+                heldObject = null;
+            }
+
+            isHolding = false;
+        }
     }
-}
 }
