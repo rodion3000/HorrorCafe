476e98b00004cc4848a4820a3b70d743c665fcb2
Rodion3000 <rodion.papizh@mail.ru>
2025-10-28 10:21:18 +0500

    1

--- Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
+++ Assets/Project/Dev/GamePlay/NPC/Player1/HeroInteraction.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,12 +6,20 @@ namespace Project.Dev.GamePlay.NPC.Player1
 {
     public class HeroInteraction : MonoBehaviour
     {
-        [SerializeField] private Transform holdPoint;
         [SerializeField] private float pickupDistance = 3f;
         [SerializeField] private LayerMask interactableLayer;
+        [SerializeField] private float jointSpring = 5000f; // сила притяжения
+        [SerializeField] private float jointDamping = 100f; // сглаживание
 
         private Rigidbody heldObject;
+        private ConfigurableJoint heldJoint;
         private bool isHolding;
+        private Camera cam;
+
+        private void Start()
+        {
+            cam = Camera.main;
+        }
 
         void Update()
         {
@@ -22,20 +31,52 @@ namespace Project.Dev.GamePlay.NPC.Player1
                     TryPickupObject();
             }
 
+            // обновляем точку привязки (камера)
+            if (isHolding && heldJoint != null)
+            {
+                heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * 1.5f;
+            }
         }
 
         private void TryPickupObject()
         {
-            Transform camTransform = Camera.main.transform;
-            Ray ray = new Ray(camTransform.position, camTransform.forward);
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+
             if (Physics.Raycast(ray, out RaycastHit hit, pickupDistance, interactableLayer))
             {
                 Rigidbody rb = hit.collider.attachedRigidbody;
                 if (rb != null)
                 {
                     heldObject = rb;
-                    heldObject.isKinematic = true;
-                    heldObject.transform.SetParent(camTransform);
+                    heldObject.useGravity = true;
+                    heldObject.isKinematic = false;
+
+                    // создаём joint НА ПРЕДМЕТЕ
+                    heldJoint = heldObject.gameObject.AddComponent<ConfigurableJoint>();
+                    heldJoint.connectedBody = null; // соединяем не с rigidbody, а с world anchor
+                    heldJoint.autoConfigureConnectedAnchor = false;
+                    heldJoint.connectedAnchor = cam.transform.position + cam.transform.forward * 1.5f;
+
+                    // блокируем вращение, но оставляем движение
+                    heldJoint.xMotion = ConfigurableJointMotion.Limited;
+                    heldJoint.yMotion = ConfigurableJointMotion.Limited;
+                    heldJoint.zMotion = ConfigurableJointMotion.Limited;
+                    heldJoint.angularXMotion = ConfigurableJointMotion.Locked;
+                    heldJoint.angularYMotion = ConfigurableJointMotion.Locked;
+                    heldJoint.angularZMotion = ConfigurableJointMotion.Locked;
+
+                    // настраиваем мягкую пружину
+                    JointDrive drive = new JointDrive
+                    {
+                        positionSpring = jointSpring,
+                        positionDamper = jointDamping,
+                        maximumForce = Mathf.Infinity
+                    };
+
+                    heldJoint.xDrive = drive;
+                    heldJoint.yDrive = drive;
+                    heldJoint.zDrive = drive;
+
                     isHolding = true;
                 }
             }
@@ -43,13 +84,18 @@ namespace Project.Dev.GamePlay.NPC.Player1
 
         private void DropObject()
         {
+            if (heldJoint != null)
+            {
+                Destroy(heldJoint);
+            }
+
             if (heldObject != null)
             {
-                heldObject.isKinematic = false;
-                heldObject.transform.SetParent(null);
+                heldObject.useGravity = true;
                 heldObject = null;
-                isHolding = false;
             }
+
+            isHolding = false;
         }
     }
 }
